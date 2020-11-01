1a7f6dcd7388f6f7e12335944936f494b199ec9f
fu6as <alvi.istamalov@gmail.com>
2020-11-01 21:37:39 +0100

    fixed SecureSessionPeer

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -1,3 +1,58 @@
+const nacl = require('libsodium-wrappers')
+const Decryptor = require('./Decryptor.js');
+const Encryptor = require('./Encryptor.js');
+
+let result = {}
+
+module.exports = async (peer) => {
+    await nacl.ready;
+    
+    let encryptor;
+    let decryptor;
+
+    let keys = nacl.crypto_kx_keypair();
+
+    if(peer){  
+        await peer.setClientSession(keys.publicKey);
+        let serverSessionKeys = nacl.crypto_kx_server_session_keys(keys.publicKey, keys.privateKey, peer.publicKey);
+  
+        encryptor = await Encryptor(serverSessionKeys.sharedTx);
+        decryptor = await Decryptor(serverSessionKeys.sharedRx);
+
+    }
+    return Object.freeze({
+        publicKey : keys.publicKey,
+        encrypt : (message) => {
+            return encryptor.encrypt(message);
+        },
+
+        decrypt : (ciphertext, nonce) => {
+            return decryptor.decrypt(ciphertext, nonce);
+        },
+
+        send : (message) => {
+            result = encryptor.encrypt(message);
+        },
+
+        receive : () => {
+            let message;
+            if(result){
+                message = decryptor.decrypt(result.ciphertext, result.nonce);
+                result = {};
+            }
+            return message;
+        },
+
+        setClientSession : async (serverPublicKey) => {
+            let clientSessionKeys =  nacl.crypto_kx_client_session_keys(keys.publicKey, keys.privateKey, serverPublicKey);
+            encryptor = await Encryptor(clientSessionKeys.sharedTx);
+            decryptor = await Decryptor(clientSessionKeys.sharedRx);
+        }
+    });
+}
+
+
+/*
 const nacl = require('libsodium-wrappers');
 
 module.exports = async () => {
@@ -8,11 +63,6 @@ module.exports = async () => {
     publicKey = keypair.publicKey;
     privateKey = keypair.privateKey;
 
-    /*
-    const sharedKeys = nacl.crypto_kx_server_session_keys(serverPublicKey, serverPrivateKey, clientPublicKey);
-    rx = sharedKeys.sharedRx;
-    tx = sharedKeys.sharedTx;
-     */
 
     return Object.freeze({
         publicKey: publicKey,
@@ -34,43 +84,4 @@ module.exports = async () => {
 
         }
     });
-}
-
-/*
-const nacl = require('libsodium-wrappers')
-
-module.exports = async () => {
-
-    // crypto_secretbox_open_easy
-
-    return Object.freeze({
-        publicKey: 'SomeKey',
-        encrypt: (msg) => {
-            // returns object with cipherText & nonce
-
-            const nonce = 'testnoncecxxxddd';
-
-
-
-            // crypto_secretbox_easy
-            const idk = nacl.crypto_secretbox_easy(msg, nonce, this.publicKey)
-
-
-            console.log(JSON.stringify(idk))
-
-            process.exit()
-
-            return {
-                nonce: nonce,
-                cipherText: ''
-            }
-        },
-        send: (peerMsg) => {
-
-        },
-        receive: () => {
-
-        }
-    });
-}
-*/
+}*/
